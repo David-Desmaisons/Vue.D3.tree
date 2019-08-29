29ecb57df4bd936d3e37cce5b35722abaf9b1eb3
David <desmaisons_david@hotmail.com>
2019-08-29 13:24:16 -0300

    Exposing behaviorMixin

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import tree from './Tree'
 import hierarchicalEdgeBundling from './HierarchicalEdgeBundling'
 import collapseOnClick from './behaviors/CollapseOnClick'
 import selectOnTextClick from './behaviors/SelectOnTextClick'
+import behaviorMixin from './behaviors/behaviorMixin'
 
 export {
     tree,
     hierarchicalEdgeBundling,
     collapseOnClick,
-    selectOnTextClick
+    selectOnTextClick,
+    behaviorMixin
 }
