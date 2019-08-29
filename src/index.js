e98b726ffbff5bea6b78a4400fd9adca1e944dd0
David <desmaisons_david@hotmail.com>
2019-08-29 13:18:40 -0300

    Exporting behaviors components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import tree from './Tree'
 import hierarchicalEdgeBundling from './HierarchicalEdgeBundling'
+import collapseOnClick from './behaviors/CollapseOnClick'
+import selectOnTextClick from './behaviors/SelectOnTextClick'
 
 export {
     tree,
-    hierarchicalEdgeBundling
+    hierarchicalEdgeBundling,
+    collapseOnClick,
+    selectOnTextClick
 }
